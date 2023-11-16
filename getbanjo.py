73478545d70b4669408a19bc2330cd35d1df5d1b
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2023-11-16 15:02:48 +0100

    [install script] add run function

--- getbanjo.py
+++ getbanjo.py
@@ -19,49 +19,54 @@ OPERATING_SYSTEMS = {
 
 BASE_URL = "https://github.com/Chnoblouch/banjo-releases/releases/latest/download"
 
-arch = ARCHITECTURES[platform.machine().lower()]
-os = OPERATING_SYSTEMS[platform.system().lower()]
-target = f"{arch}-{os}"
+def run():
+    arch = ARCHITECTURES[platform.machine().lower()]
+    os = OPERATING_SYSTEMS[platform.system().lower()]
+    target = f"{arch}-{os}"
 
-print(f"Platform: {target}")
+    print(f"Platform: {target}")
 
-install_dir = Path.home() / ".banjo"
-if not install_dir.exists():
-    install_dir.mkdir()
+    install_dir = Path.home() / ".banjo"
+    if not install_dir.exists():
+        install_dir.mkdir()
 
-print(f"Install directory: {install_dir}")
+    print(f"Install directory: {install_dir}")
 
-zip_name = f"banjo-{target}.zip"
-url = f"{BASE_URL}/{zip_name}"
-print(f"Download URL: {url}")
+    zip_name = f"banjo-{target}.zip"
+    url = f"{BASE_URL}/{zip_name}"
+    print(f"Download URL: {url}")
 
-print(f"\nDownloading...")
+    print(f"\nDownloading...")
 
-zip_path = install_dir / zip_name
-urllib.request.urlretrieve(url, zip_path)
+    zip_path = install_dir / zip_name
+    urllib.request.urlretrieve(url, zip_path)
 
-print(f"Extracting...")
+    print(f"Extracting...")
 
-zip_ref = ZipFile(zip_path, "r")
+    zip_ref = ZipFile(zip_path, "r")
 
-for member in zip_ref.filelist:
-    prefix = f"banjo-{target}/"
-    if member.filename == prefix or not member.filename.startswith(prefix):
-        continue
+    for member in zip_ref.filelist:
+        prefix = f"banjo-{target}/"
+        if member.filename == prefix or not member.filename.startswith(prefix):
+            continue
 
-    member.filename = member.filename[len(prefix):]
-    zip_ref.extract(member, install_dir)
+        member.filename = member.filename[len(prefix):]
+        zip_ref.extract(member, install_dir)
 
-zip_ref.close()
-zip_path.unlink()
+    zip_ref.close()
+    zip_path.unlink()
 
-print("All done!")
+    print("All done!")
 
-print("\nRun this to add banjo to your path:")
+    print("\nRun this to add banjo to your path:")
 
-bin_dir = install_dir / "bin"
+    bin_dir = install_dir / "bin"
 
-if os == "windows":
-    print(f"$ENV:PATH=\"$ENV:PATH;{bin_dir}\"")
-else:
-    print(f"export PATH=\"$PATH:{bin_dir}\"")
+    if os == "windows":
+        print(f"$ENV:PATH=\"$ENV:PATH;{bin_dir}\"")
+    else:
+        print(f"export PATH=\"$PATH:{bin_dir}\"")
+
+
+if __name__ == "__main__":
+    run()
